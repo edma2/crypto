29880525abc5baac50844dd664d075d94116c907
edma2 <edma2@san.rr.com>
2011-01-09 12:12:54 -0800

    rename

--- sha1.c
+++ sha1.c
@@ -1,4 +1,4 @@
-/* sha1sum.c - calculate the SHA1 sum of a file, prints to stdout */
+/* sha1.c - calculate the SHA1 sum of a file, prints to stdout */
 #include <openssl/sha.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
                 if (argc == 3)
                         flag = !strcmp(argv[1], "-c");
         } else {
-                fprintf(stderr, "Usage: sha1sum [flag] <file>...\n");
+                fprintf(stderr, "Usage: sha1 [flag] <file>...\n");
                 return -1;
         }
 
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
                 /* Open the checksum file for checking */
                 fp = fopen(argv[2], "r");
                 if (fp == NULL) {
-                        fprintf(stderr, "sha1sum: error opening file %s\n", argv[2]);
+                        fprintf(stderr, "sha1: error opening file %s\n", argv[2]);
                         return -1;
                 }
                 while (fgets(line, BUFSIZE, fp) > 0) {
@@ -45,14 +45,14 @@ int main(int argc, char *argv[]) {
                         /* Open each file specified in the checksum file */
                         fp_check = fopen(check_path, "r");
                         if (fp_check == NULL) {
-                                fprintf(stderr, "sha1sum: error opening file %s\n", check_path);
+                                fprintf(stderr, "sha1: error opening file %s\n", check_path);
                                 fclose(fp);
                                 return -1;
                         }
                         if (hash_file(fp_check, hash) < 0) {
                                 fclose(fp_check);
                                 fclose(fp);
-                                fprintf(stderr, "sha1sum: error hashing file\n");
+                                fprintf(stderr, "sha1: error hashing file\n");
                                 return -1;
                         }
                         /* Compare previous hash with current hash */
@@ -72,12 +72,12 @@ int main(int argc, char *argv[]) {
                         /* Open file for reading */
                         fp = fopen(argv[r], "r");
                         if (fp == NULL) {
-                                fprintf(stderr, "sha1sum: error opening file %s\n", argv[r]);
+                                fprintf(stderr, "sha1: error opening file %s\n", argv[r]);
                                 return -1;
                         }
                         if (hash_file(fp, hash) < 0) {
                                 fclose(fp);
-                                fprintf(stderr, "sha1sum: error hashing file\n");
+                                fprintf(stderr, "sha1: error hashing file\n");
                                 return -1;
                         }
                         printf("%s  %s\n", hash, argv[r]);
@@ -90,40 +90,30 @@ int main(int argc, char *argv[]) {
 
 /* Store hash bytes in hash, return -1 on error, 0 otherwise */
 int hash_file(FILE *fp, char *hash) {
-        SHA_CTX *ctx;
+        SHA_CTX ctx;
         uint8_t buf[BUFSIZE];
         uint8_t raw_hash[HASHSIZE];
         char byte[2];
         int count;
         int i, j;
 
-        /* Initialize sha1sum structure */
-        ctx = malloc(sizeof(SHA_CTX));
-        if (ctx == NULL) {
-                fprintf(stderr, "sha1sum: malloc() failed\n");
-                return -1;
-        }
-        if (!SHA1_Init(ctx)) {
-                fprintf(stderr, "sha1sum: could not initialize SHA_CTX struct\n");
-                free(ctx);
+        if (!SHA1_Init(&ctx)) {
+                fprintf(stderr, "sha1: could not initialize SHA_CTX struct\n");
                 return -1;
         }
 
         /* Pass BUFSIZE bytes at a time to struct */
         while ((count = fread(buf, sizeof(uint8_t), BUFSIZE, fp)) > 0) {
                 /* Pass count bytes from buf to struct */
-                if (!SHA1_Update(ctx, buf, count)) {
-                        fprintf(stderr, "sha1sum: could not update struct SHA_CTX\n");
-                        free(ctx);
+                if (!SHA1_Update(&ctx, buf, count)) {
+                        fprintf(stderr, "sha1: could not update struct SHA_CTX\n");
                         return -1;
                 }
         }
-        if (!SHA1_Final(raw_hash, ctx)) {
-                fprintf(stderr, "sha1sum: could not finalize struct SHA_CTX\n");
-                free(ctx);
+        if (!SHA1_Final(raw_hash, &ctx)) {
+                fprintf(stderr, "sha1: could not finalize struct SHA_CTX\n");
                 return -1;
         }
-        free(ctx);
 
         /* Write to string */
         for (i = 0, j = 0; i < HASHSIZE; i++) {
