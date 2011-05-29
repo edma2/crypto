d3492192925470942fce8eaab9cf624b0a491ebf
Eugene Ma <edma2@Section9.localdomain>
2011-05-28 21:58:26 -0700

    overwrite first byte instead of tmp file

--- filelock.c
+++ filelock.c
@@ -21,17 +21,6 @@ typedef union {
         uint8_t iv8[sizeof(uint64_t)];
 } IVec;
 
-void clean(int mode, FILE *fp_in, FILE *fp_out, FILE *fp_tmpout) {
-        fclose(fp_in);
-        fclose(fp_out);
-        /* Close and remove temporary file, if it in encrypt mode */
-        if (mode == BF_ENCRYPT) {
-                fclose(fp_tmpout);
-                if (remove(TMP_PATH) < 0)
-                        fprintf(stderr, "Error: unable to delete temporary file\n");
-        }
-}
-
 int main(int argc, char *argv[]) {
         char c;
         uint8_t pw[PW_SIZE];
@@ -41,14 +30,11 @@ int main(int argc, char *argv[]) {
         int mode;
         int read_count, write_count;
         int i;
-        FILE *fp_in, *fp_out, *fp_tmpout;
+        FILE *fin, *fout;
         time_t tm;
         IVec iv;
         BF_KEY key;
         struct termios term, term_orig;
-
-        uint8_t buf[8];
-        int count;
 
         /* Check correct argument count */
         if (argc != 4) {
@@ -63,56 +49,33 @@ int main(int argc, char *argv[]) {
         }
         mode = (c == 'e') ? BF_ENCRYPT : BF_DECRYPT;
 
-        /*****************************************************************************/
-        /* Open necessary files */
-        /*****************************************************************************/
-
-        /* Set input input stream to stdin or file */
-        if (argv[2][0] == '-' && argv[2][1] == '\0') {
+        /* Open input stream from stdin or file */
+        if (argv[2][0] != '\0' && argv[2][0] == '-' && argv[2][1] == '\0') {
                 /* Use stdin */
-                fp_in = stdin;
+                fin = stdin;
         } else {
                 /* Open file for reading */
-                fp_in = fopen(argv[2], "r");
-                if (fp_in == NULL) {
+                fin = fopen(argv[2], "r");
+                if (fin == NULL) {
                         fprintf(stderr, "Error: unable to open %s for reading\n", argv[2]);
                         return -1;
                 }
         }
-        
+
         /* Set output stream to stdout or file */
         if (argv[3][0] == '-' && argv[3][1] == '\0') {
                 /* Use stdout */
-                fp_out = stdout;
+                fout = stdout;
         } else {
                 /* Open file for writing */
-                fp_out = fopen(argv[3], "w+");
-                if (fp_out == NULL) {
+                fout = fopen(argv[3], "w+");
+                if (fout == NULL) {
                         fprintf(stderr, "Error: unable to open %s for writing\n", argv[3]);
-                        fclose(fp_in);
-                        return -1;
-                }
-
-        }
-
-        /* 
-         * If encrypt, open a temporary file to write to. Later we will 
-         * write this to the real output file. Necessary because if input 
-         * option is stdin we cannot know file length beforehand.
-         */
-        if (mode == BF_ENCRYPT) {
-                fp_tmpout = fopen(TMP_PATH, "w+");
-                if (fp_tmpout == NULL) {
-                        fprintf(stderr, "Error: unable to open %s for writing\n", TMP_PATH);
-                        fclose(fp_in);
-                        fclose(fp_out);
+                        if (fin != stdin)
+                                fclose(fin);
                         return -1;
                 }
         }
-
-        /*****************************************************************************/
-        /* Get password from user */
-        /*****************************************************************************/
 
         /* Disable terminal echo */
         tcgetattr(STDIN_FILENO, &term);
@@ -131,12 +94,21 @@ int main(int argc, char *argv[]) {
         /* Initialize the key */
         BF_set_key(&key, strlen((char *)pw), pw);
 
-        /*****************************************************************************/
-        /* Prepare for encryption or decryption */
-        /*****************************************************************************/
-
         /*  Generate and write initialization vector */
         if (mode == BF_ENCRYPT) {
+                /* Write dummy 0 byte first. We overwrite it later after we know last block size */
+                c = 0;
+                if (fwrite(&c, sizeof(uint8_t), 1, fout) != 1) {
+                        fprintf(stderr, "Error: unable to write initialization vector\n");
+                        if (fin != stdin)
+                                fclose(fin);
+                        if (fout != stdout) {
+                                fclose(fout);
+                                remove(argv[3]);
+                        }
+                        return -1;
+                }
+
                 /* Seed random number generator with unix time and pass random number to IV */
                 tm = time(NULL);
                 srandom(tm);
@@ -144,40 +116,48 @@ int main(int argc, char *argv[]) {
                 /* Use all 64 bits of initialization vector by ORing 2 32-bit random ints */
                 iv.iv64 = ((uint64_t)random() << 32) | random();
 
-                /* Write IV to tmp file */
-                if (fwrite(iv.iv8, sizeof(uint8_t), IV_SIZE, fp_tmpout) != IV_SIZE) {
+                /* Write initialization vector */
+                if (fwrite(iv.iv8, sizeof(uint8_t), IV_SIZE, fout) != IV_SIZE) {
                         fprintf(stderr, "Error: unable to write initialization vector\n");
-                        clean(mode, fp_in, fp_out, fp_tmpout);
+                        if (fin != stdin)
+                                fclose(fin);
+                        if (fout != stdout) {
+                                fclose(fout);
+                                remove(argv[3]);
+                        }
                         return -1;
                 }
         } else if (mode == BF_DECRYPT) {
                 /* Recover offset information */
-                if (fread(&last_block_size, sizeof(uint8_t), 1, fp_in) < 0) {
+                if (fread(&last_block_size, sizeof(uint8_t), 1, fin) < 0) {
                         fprintf(stderr, "Error: unable to read offset\n");
-                        clean(mode, fp_in, fp_out, fp_tmpout);
+                        if (fin != stdin)
+                                fclose(fin);
+                        if (fout != stdout) {
+                                fclose(fout);
+                                remove(argv[3]);
+                        }
                         return -1;
                 }
 
                 /* Recover initialization vector */
-                if (fread(iv.iv8, sizeof(uint8_t), IV_SIZE, fp_in) < 0) {
+                if (fread(iv.iv8, sizeof(uint8_t), IV_SIZE, fin) < 0) {
                         fprintf(stderr, "Error: unable to read initialization vector\n");
-                        clean(mode, fp_in, fp_out, fp_tmpout);
+                        if (fin != stdin)
+                                fclose(fin);
+                        if (fout != stdout) {
+                                fclose(fout);
+                                remove(argv[3]);
+                        }
                         return -1;
                 }
         }
 
-        /*****************************************************************************/
-        /* Main loop */
-        /*****************************************************************************/
-
-        /* If decrypt, just write to fp_out */
-        if (mode == BF_DECRYPT)
-                fp_tmpout = fp_out;
-
-        /* Delayed write - skip first iteration */
+        /* Delay write by skipping first iteration */
         write_count = 0;
 
-        while ((read_count = fread(buf_in, sizeof(uint8_t), BF_BLOCK_SIZE, fp_in)) > 0) {
+        /* Main loop */
+        while ((read_count = fread(buf_in, sizeof(uint8_t), BF_BLOCK_SIZE, fin)) > 0) {
                 /* Add necessary padding and record offset on last block */
                 if (mode == BF_ENCRYPT) {
                         last_block_size = read_count;
@@ -185,9 +165,14 @@ int main(int argc, char *argv[]) {
                                 buf_in[read_count] = 0;
                 }
 
-                if (fwrite(buf_out, sizeof(uint8_t), write_count, fp_tmpout) != write_count) {
+                if (fwrite(buf_out, sizeof(uint8_t), write_count, fout) != write_count) {
                         fprintf(stderr, "Error: write error\n");
-                        clean(mode, fp_in, fp_out, fp_tmpout);
+                        if (fin != stdin)
+                                fclose(fin);
+                        if (fout != stdout) {
+                                fclose(fout);
+                                remove(argv[3]);
+                        }
                         break;
                 }
 
@@ -203,34 +188,37 @@ int main(int argc, char *argv[]) {
 
         /* Must write last block since reads lead writes by 1 iteration */
         write_count = ((mode == BF_ENCRYPT) ? BF_BLOCK_SIZE : last_block_size);
-        if (fwrite(buf_out, sizeof(uint8_t), write_count, fp_tmpout) != write_count) {
-                clean(mode, fp_in, fp_out, fp_tmpout);
+        if (fwrite(buf_out, sizeof(uint8_t), write_count, fout) != write_count) {
                 fprintf(stderr, "Error: write error\n");
+                if (fin != stdin)
+                        fclose(fin);
+                if (fout != stdout) {
+                        fclose(fout);
+                        remove(argv[3]);
+                }
                 return -1;
         }
 
-        /* 
-         * If encrypt, write offset information to fp_out 
-         * and transfer contents of fp_tmpout to fp_out.
-         */
+        /* Rewind to beginning and overwrite dummy byte with last_block_size */
         if (mode == BF_ENCRYPT) {
-                /* Write offset as first byte of output file */
-                if (fwrite(&last_block_size, sizeof(uint8_t), 1, fp_out) < 0) {
-                        fprintf(stderr, "Error: unable to write offset\n");
-                        clean(mode, fp_in, fp_out, fp_tmpout);
-                        return -1;
-                }
-
-                /* Copy file contents of temporary file to real output file */
-                rewind(fp_tmpout);
-                while ((count = fread(buf, sizeof(uint8_t), 8, fp_tmpout)) > 0) {
-                        if (fwrite(buf, sizeof(uint8_t), 8, fp_out) != count) {
-                                fprintf(stderr, "Error: write error\n");
-                                break;
+                rewind(fout);
+                if (fwrite(&last_block_size, sizeof(uint8_t), 1, fout) != 1) {
+                        fprintf(stderr, "Error: unable to write initialization vector\n");
+                        if (fin != stdin)
+                                fclose(fin);
+                        if (fout != stdout) {
+                                fclose(fout);
+                                remove(argv[3]);
                         }
+                        return -1;
                 }
         }
 
-        clean(mode, fp_in, fp_out, fp_tmpout);
+        /* Clean up */
+        if (fin != stdin)
+                fclose(fin);
+        if (fout != stdout)
+                fclose(fout);
+
         return 0;
 }
